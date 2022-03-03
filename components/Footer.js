75c2f37d4c03d7564f3166065c07de0044417a80
Joonas Heimonen <heimonenjoonas@gmail.com>
2022-03-03 18:02:04 +0200

    Update

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from '../style/style';
+
+export default function Header() {
+  return (
+    <View style={styles.footer}>
+      <Text style={styles.author}>Author: Joonas Heimonen</Text>
+    </View>
+  );
+}
